650a6069fcdcb86f05c5869cb7f309f273547115
Aryan <aryan.r.t@hotmail.com>
2020-06-21 10:56:00 -0400

    Created Spring MVC project to start with

---
+++ src/main/java/com/aryanrt/stats/RestNbaApplication.java
@@ -0,0 +1,13 @@
+package com.aryanrt.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestNbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestNbaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aryanrt/stats/controllers/GeneralController.java
@@ -0,0 +1,10 @@
+package com.aryanrt.stats.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller()
+public class GeneralController 
+{
+
+}
