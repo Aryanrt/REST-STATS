c2140d8c9dd94e025448a23d19a1d5211762cec0
Aryan <aryan.r.t@hotmail.com>
2020-06-28 21:59:58 -0400

    Player And PlayerPK Entites and Repository is created. Player Controller coming up

---
+++ src/main/java/com/aryanrt/stats/repositories/PlayerRepository.java
@@ -0,0 +1,12 @@
+package com.aryanrt.stats.repositories;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.aryanrt.stats.models.Player;
+import com.aryanrt.stats.models.PlayerPK;
+
+
+@RepositoryRestResource(collectionResourceRel = "player", path = "player")
+public interface PlayerRepository extends PagingAndSortingRepository<Player, PlayerPK> {
+}
