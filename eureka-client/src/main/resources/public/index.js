0bc0f24797a6ce6c59d10594ccbf3a770a4c2d20
aryan <aryan.r.t@hotmail.com>
2020-08-02 11:59:23 -0400

    Input Hightlights improvement

--- eureka-client/src/main/resources/public/index.js
+++ eureka-client/src/main/resources/public/index.js
@@ -1,14 +1,17 @@
 function myFunction() {
   // Get the checkbox
   document.getElementById("v2-lable").style.display="none";
-  document.getElementById("v2-div").style.display="none";
-
+  document.getElementById("v2-div1").style.display="none";
+  document.getElementById("v2-div2").style.display="none";
 
   document.getElementById("v1-lable").style.display="block";
   document.getElementById("v1-div").style.display="block";
   document.getElementById("v1-lable").style.visibility="visible";
   document.getElementById("v1-div").style.visibility="visible";
-  document.getElementById("signle-date").click();
+
+  unsetHighlight(document.getElementById("d2"));
+  setHighlight(document.getElementById("d1"));
+  document.getElementById("signle-date").checked=true;
 
 
 
@@ -19,11 +22,14 @@ function myFunction2() {
   document.getElementById("v1-div").style.display="none";
 
   document.getElementById("v2-lable").style.display="block";
-  document.getElementById("v2-div").style.display="block";
+  document.getElementById("v2-div1").style.display="block";
+  document.getElementById("v2-div2").style.display="block";
 
-console.log("aaaaaaaaaaaaaaaaaaaaaaaaaa");
-document.getElementById("mutli-dates").click();
+ unsetHighlight(document.getElementById("d1"));
+ setHighlight(document.getElementById("d2"));
+ document.getElementById("mutli-dates").checked= true;
 } 
+
 function teamSelector(){
   document.getElementById("teams-lable").style.display="block";
   document.getElementById("teams-div").style.display="block";
@@ -45,7 +51,7 @@ function mutiTeams(){
 
 function toDate(id)
 {
-	document.getElementById(id).type="date";		
+	//document.getElementById(id).type="date";		
 }
 function selectTeam(id)
 {
@@ -53,35 +59,18 @@ function selectTeam(id)
 	if( document.getElementById(id).getElementsByTagName("input")[0].checked )
 	{
 		document.getElementById(id).style.backgroundColor = "#e6e6e6";
-		
-		var children = document.getElementById("teams").children;
-		for(i=0; i< children.length;i++)
-		{
-			if(children[i].id == "view"+id)
-			{
-				document.getElementById("teams").removeChild(children[i]);
-				break;
-			}
-		}
 		document.getElementById(id).getElementsByTagName("input")[0].checked=false;
 	}
 	else
 	{
 		document.getElementById(id).style.backgroundColor = "#00ad5f";
-
-		var temp = document.createElement("lable");
-		temp.id = "view"+id;
-		temp.className="label-checkbox100";
-		temp.style.backgroundColor="#00ad5f";
-		temp.textContent=document.getElementById(id).textContent;
-		document.getElementById("teams").appendChild(temp);
 		document.getElementById(id).getElementsByTagName("input")[0].checked=true;
 	}
 		
 }
 function showTeams(id)
 {
-	if(  document.getElementById(id).checked )
+	/*if(  document.getElementById(id).checked )
 	{
 		var temp = document.createElement("lable");
 		temp.id = "view"+id;
@@ -101,6 +90,22 @@ function showTeams(id)
 			}
 		}
 		alert(4);
-	}	
+	}	*/
+}
+function evalHightlight(element)
+{
+	if(element.getElementsByTagName("input")[0].value == "")
+		unsetHighlight(element);
+	else
+		setHighlight(element); 
+	
+}
+function setHighlight(element)
+{
+	element.setAttribute("val","set");	
+}
+function unsetHighlight(element)
+{
+	element.setAttribute("val","unset");	
 }
 
