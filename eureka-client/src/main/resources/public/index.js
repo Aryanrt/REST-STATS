03d27741851a85cd88361094675a97c1030c13b5
aryan <aryan.r.t@hotmail.com>
2020-08-02 18:37:55 -0400

    Form UI improvements

--- eureka-client/src/main/resources/public/index.js
+++ eureka-client/src/main/resources/public/index.js
@@ -36,36 +36,42 @@ function teamSelector(){
   document.getElementById("multi-teams").click;
 }
 function allTeams(){
-  document.getElementById("all-teams").click();
+  document.getElementById("all-teams").checked=true;
   document.getElementById("teams-lable").style.visibility="hidden";
   document.getElementById("teams-div").style.visibility="hidden";
-  document.getElementById("teams").textContent = "All teams";
+
+  unsetHighlight(document.getElementById("div4"));
+  setHighlight(document.getElementById("div3"));
 
 }
 function mutiTeams(){
-	document.getElementById("mutli-teams").click();
+	document.getElementById("mutli-teams").checked = true;
 	document.getElementById("teams-lable").style.visibility="visible";
 	document.getElementById("teams-div").style.visibility="visible";
-	document.getElementById("teams").textContent = "";
+
+	unsetHighlight(document.getElementById("div3"));
+    setHighlight(document.getElementById("div4"));
 }
 
 function toDate(id)
 {
 	//document.getElementById(id).type="date";		
 }
-function selectTeam(id)
+function selectTeam(element)
 {
 
-	if( document.getElementById(id).getElementsByTagName("input")[0].checked )
+	if( element.getElementsByTagName("input")[0].checked )
 	{
-		document.getElementById(id).style.backgroundColor = "#e6e6e6";
-		document.getElementById(id).getElementsByTagName("input")[0].checked=false;
+		element.style.backgroundColor = "#e6e6e6";
+		element.getElementsByTagName("input")[0].checked=false;
 	}
 	else
 	{
-		document.getElementById(id).style.backgroundColor = "#00ad5f";
-		document.getElementById(id).getElementsByTagName("input")[0].checked=true;
+		//document.getElementById(id).style.backgroundColor = "#00ad5f";
+		element.getElementsByTagName("input")[0].checked=true;
+		
 	}
+	evalHightlight(element);
 		
 }
 function showTeams(id)
@@ -93,12 +99,26 @@ function showTeams(id)
 	}	*/
 }
 function evalHightlight(element)
-{
-	if(element.getElementsByTagName("input")[0].value == "")
-		unsetHighlight(element);
-	else
-		setHighlight(element); 
+{	
 	
+	if(element.tagName == "DIV")
+	{
+		
+		if(element.getElementsByTagName("input")[0].value == "")
+			unsetHighlight(element);
+		else
+			setHighlight(element); 
+	}
+	
+	else if(element.tagName == "LABEL")
+	{
+		
+		if(! element.getElementsByTagName("input")[0].checked)
+			unsetHighlight(element);
+		else
+			setHighlight(element); 
+	}
+		
 }
 function setHighlight(element)
 {
@@ -107,5 +127,41 @@ function setHighlight(element)
 function unsetHighlight(element)
 {
 	element.setAttribute("val","unset");	
+}
+function evalDate(element)
+{
+	var fromDate = new Date(document.getElementById("dates-from").value);
+	var toDate = new Date(document.getElementById("dates-to").value);
+	evalHightlight(element);
+	if(fromDate  > toDate )
+	{
+		
+		document.getElementById("v2-div1").setAttribute("val","error");
+		document.getElementById("v2-div2").setAttribute("val","error");
+		document.getElementById("error-msg").textContent = " Ending Day Can Not Be Before The Starting Day";
+		document.getElementById("error-msg").style.visibility = "visible";
+		document.getElementById("submit-button").disabled = true;
+		
+	}
+	else if( new Date(fromDate.getTime() + 7 * 24 * 60 * 60 * 1000) < toDate)
+	{
+		document.getElementById("v2-div1").setAttribute("val","error");
+		document.getElementById("v2-div2").setAttribute("val","error");
+		document.getElementById("error-msg").textContent = " In Trial Mode, The Maximum Date Range Is 1 Week";
+		document.getElementById("error-msg").style.visibility = "visible";
+		document.getElementById("submit-button").disabled = true;
+	}
+	else
+	{
+		if( document.getElementById("v2-div1").getAttribute("val") == "error")
+			document.getElementById("v2-div1").setAttribute("val","set");
+		if( document.getElementById("v2-div2").getAttribute("val") == "error")
+			document.getElementById("v2-div2").setAttribute("val","set");
+		
+		document.getElementById("error-msg").textContent = "";
+		document.getElementById("error-msg").style.visibility = "hidden";	
+		document.getElementById("submit-button").disabled = false;	
+	}
+
 }
 
